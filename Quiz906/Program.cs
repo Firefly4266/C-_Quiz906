285d8aa7dd2711a7efb2c57daa8f342402def61c
Firefly4266 <gaines.johnp@gmail.com>
2017-09-06 12:21:00 -0700

    Added question

--- Quiz906/Program.cs
+++ Quiz906/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 3)	Write a program that contains two structures.One for an employee and
+        one for their birthdate.Then write a class that will use the structs to
+        gather data for two employees stored in an array.Print the data out on
+        the screen in a single sentence.*/
+
 namespace Quiz906
 {
     class Program
