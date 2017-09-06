d93a8e181aca45a4e3115929b65b6be313d04e1f
Firefly4266 <gaines.johnp@gmail.com>
2017-09-06 12:18:31 -0700

    Added question

--- Quiz906/Program.cs
+++ Quiz906/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 1)	Write a program that will ask the user to supply a sentence. Return that
+        sentence back to the user with each character separated by a space. */
+
 namespace Quiz906
 {
     class Program
