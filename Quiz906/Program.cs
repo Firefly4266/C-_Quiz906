0b4c3266619a4ff63bfc693faf268c47ae461016
Firefly4266 <gaines.johnp@gmail.com>
2017-09-06 12:19:13 -0700

    added question

--- Quiz906/Program.cs
+++ Quiz906/Program.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 2)   Write a simple structure that contains 2 integer variables.Create a
+        class that will use those two variables by initializing them then
+        summing them. */
+
 namespace Quiz906
 {
     class Program
