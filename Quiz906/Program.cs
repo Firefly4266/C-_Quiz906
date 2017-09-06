6b8909d26fef79a0fcffaa86f0242724a793b3c8
Firefly4266 <gaines.johnp@gmail.com>
2017-09-06 12:22:49 -0700

    Added question

--- Quiz906/Program.cs
+++ Quiz906/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 4)	Write a program that will accept a sentence from a user.Return to the
+        user the number of vowels and consonants within that string.  Be sure to
+        account for capitalization within the sentence and exclude all
+        punctuation from the counts.*/
+
 namespace Quiz906
 {
     class Program
